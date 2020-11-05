6963a530b108c52a6611373922981e4f0043b93a
Philip Wang <yutong2@ualberta.ca>
2020-11-04 22:29:53 -0700

    Merge weijia and zexin3.
    Solve some conflicts and revise some codes.

---
+++ code/app/src/main/java/com/example/books4share/ViewRequestActivity.java
@@ -0,0 +1,24 @@
+package com.example.books4share;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_request);
+
+        final Button backButton = findViewById(R.id.back_button2);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
